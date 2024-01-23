ac994dbcb1758ae4c9ff1ca95f143aa38a5bc00b
codegeek <brianngeorgee@gmail.com>
2024-01-23 23:02:13 +0300

    algorithms sorting

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,48 +1,68 @@
 #include "sort.h"
 
 /**
- * counting_sort - Sorts an array using the counting sort algorithm.
- * @array: The array to sort.
- * @size: The length of the array.
+ * get_max - Get the maximum value in an array of integers.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Return: The maximum integer in the array.
  */
+int get_max(int *array, int size)
+{
+	int max, i;
 
+	for (max = array[0], i = 1; i < size; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
+
+	return (max);
+}
+
+/**
+ * counting_sort - Sort an array of integers in ascending order
+ *                 using the counting sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Prints the counting array after setting it up.
+ */
 void counting_sort(int *array, size_t size)
 {
-	int *count_arr = NULL, *arr_sorted = NULL, max_val = 0;
-	size_t i;
+	int *count, *sorted, max, i;
 
-	if ((array == NULL) || (size < 2))
+	if (array == NULL || size < 2)
 		return;
-	for (i = 0; i < size; i++)
-	{
-		max_val = (array[i] > max_val ? array[i] : max_val);
-		if (array[i] < 0)
-			return;
-	}
-	count_arr = malloc(sizeof(int) * (max_val + 1));
-	if (count_arr == NULL)
+
+	sorted = malloc(sizeof(int) * size);
+	if (sorted == NULL)
 		return;
-	arr_sorted = malloc(sizeof(int) * size);
-	if (arr_sorted == NULL)
+	max = get_max(array, size);
+	count = malloc(sizeof(int) * (max + 1));
+	if (count == NULL)
 	{
-		free(count_arr);
+		free(sorted);
 		return;
 	}
-	for (i = 0; i < (size_t)(max_val + 1); i++)
-		count_arr[i] = 0;
-	for (i = 0; i < size; i++)
-		count_arr[array[i]]++;
-	for (i = 1; i < (size_t)(max_val + 1); i++)
-		count_arr[i] += count_arr[i - 1];
-	print_array(count_arr, max_val + 1);
-	for (i = size - 1; ; i--)
+
+	for (i = 0; i < (max + 1); i++)
+		count[i] = 0;
+	for (i = 0; i < (int)size; i++)
+		count[array[i]] += 1;
+	for (i = 0; i < (max + 1); i++)
+		count[i] += count[i - 1];
+	print_array(count, max + 1);
+
+	for (i = 0; i < (int)size; i++)
 	{
-		count_arr[array[i]]--;
-		arr_sorted[count_arr[array[i]]] = array[i];
-		if (i == 0)
-			break;
+		sorted[count[array[i]] - 1] = array[i];
+		count[array[i]] -= 1;
 	}
-	for (i = 0; i < size; i++)
-		array[i] = arr_sorted[i];
-	free(arr_sorted), free(count_arr);
+
+	for (i = 0; i < (int)size; i++)
+		array[i] = sorted[i];
+
+	free(sorted);
+	free(count);
 }
--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,81 +1,82 @@
 #include "sort.h"
 
+void merge_subarr(int *subarr, int *buff, size_t front, size_t mid,
+		size_t back);
+void merge_sort_recursive(int *subarr, int *buff, size_t front, size_t back);
+void merge_sort(int *array, size_t size);
+
 /**
- * top_down_merge - Merges arr that has been split with the top-down approach
- * @array: The array to merge.
- * @array_c: The temporary array.
- * @l: The left index of the split-array.
- * @m: The mid-point of the split-array.
- * @r: The right index of the split-array.
+ * merge_subarr - Sort a subarray of integers.
+ * @subarr: A subarray of an array of integers to sort.
+ * @buff: A buffer to store the sorted subarray.
+ * @front: The front index of the array.
+ * @mid: The middle index of the array.
+ * @back: The back index of the array.
  */
-
-void top_down_merge(int *array, int *array_c, size_t l, size_t m, size_t r)
+void merge_subarr(int *subarr, int *buff, size_t front, size_t mid,
+		size_t back)
 {
-	size_t a = l, b = m, c;
+	size_t i, j, k = 0;
 
-	printf("Merging...\n");
-	printf("[left]: ");
-	print_array(array + l, m - l);
+	printf("Merging...\n[left]: ");
+	print_array(subarr + front, mid - front);
+
 	printf("[right]: ");
-	print_array(array + (m), r - m);
-	for (c = l; c < r; c++)
-	{
-		if ((a < m) && ((b >= r) || (array_c[a] <= array_c[b])))
-		{
-			array[c] = array_c[a++];
-		}
-		else
-		{
-			array[c] = array_c[b++];
-		}
-	}
+	print_array(subarr + mid, back - mid);
+
+	for (i = front, j = mid; i < mid && j < back; k++)
+		buff[k] = (subarr[i] < subarr[j]) ? subarr[i++] : subarr[j++];
+	for (; i < mid; i++)
+		buff[k++] = subarr[i];
+	for (; j < back; j++)
+		buff[k++] = subarr[j];
+	for (i = front, k = 0; i < back; i++)
+		subarr[i] = buff[k++];
+
 	printf("[Done]: ");
-	print_array(array + l, r - l);
+	print_array(subarr + front, back - front);
 }
 
 /**
- * merge_sort - Sorts an array using the merge sort algorithm.
- * @array: The array to sort.
- * @size: The length of the array.
+ * merge_sort_recursive - Implement the merge sort algorithm through recursion.
+ * @subarr: A subarray of an array of integers to sort.
+ * @buff: A buffer to store the sorted result.
+ * @front: The front index of the subarray.
+ * @back: The back index of the subarray.
  */
+void merge_sort_recursive(int *subarr, int *buff, size_t front, size_t back)
+{
+	size_t mid;
 
+	if (back - front > 1)
+	{
+		mid = front + (back - front) / 2;
+		merge_sort_recursive(subarr, buff, front, mid);
+		merge_sort_recursive(subarr, buff, mid, back);
+		merge_subarr(subarr, buff, front, mid, back);
+	}
+}
+
+/**
+ * merge_sort - Sort an array of integers in ascending
+ *              order using the merge sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Implements the top-down merge sort algorithm.
+ */
 void merge_sort(int *array, size_t size)
 {
-	size_t i;
-	int *array_c = NULL;
+	int *buff;
 
-	if (array != NULL)
-	{
-		array_c = malloc(sizeof(int) * size);
-		if (array_c != NULL)
-		{
-			for (i = 0; i < size; i++)
-				array_c[i] = array[i];
-			split_merge(array, array_c, size, 0, size);
-			free(array_c);
-		}
-	}
-}
-
-/**
- * split_merge - Sorts arr that has been split using the merge sort algorithm
- * @array: The array that has been split.
- * @array_c: The temporary array.
- * @size: The length of the original array.
- * @l: The left index of the split-array.
- * @r: The right index of the split-array.
- */
-
-void split_merge(int *array, int *array_c, size_t size, size_t l, size_t r)
-{
-	size_t i, mid;
-
-	if ((r - l) <= 1)
+	if (array == NULL || size < 2)
 		return;
-	mid = (l + r) / 2;
-	split_merge(array, array_c, size, l, mid);
-	split_merge(array, array_c, size, mid, r);
-	for (i = l; i <= r + (r == size ? -1 : 0); i++)
-		array_c[i] = array[i];
-	top_down_merge(array, array_c, l, mid, r);
+
+	buff = malloc(sizeof(int) * size);
+	if (buff == NULL)
+		return;
+
+	merge_sort_recursive(array, buff, 0, size);
+
+	free(buff);
 }
